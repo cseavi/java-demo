1f9a983f26ada51f52ed4368ca6754059afc8336
Avinash <bitsavinash@gmail.com>
2020-07-23 11:58:22 +0530

    Initial project skeleton

---
+++ src/test/java/tech/dipc/javademo/JavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dipc.javademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
